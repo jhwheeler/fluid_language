4975c9d97b0bd8e7438e2410f507b9b1738a7523
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2022-05-23 13:31:29 +0800

    tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 import md from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
-  preprocess: [md.mdsvex(mdsvexConfig)],
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+    md.mdsvex(mdsvexConfig),
+  ],
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
