1de27b0aa260d8db5487279deea073613a6b04b7
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2024-05-18 21:20:57 +0800

    chore: remove unnecessary comment

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,6 @@ const config = {
     md.mdsvex(mdsvexConfig),
   ],
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     adapter: adapter(),
     prerender: {
